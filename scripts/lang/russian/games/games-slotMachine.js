e599a0a5e3544fcf4311fa05d87db6a465404247
PhantomBotRU <43908302+PhantomBotRU@users.noreply.github.com>
2019-05-14 04:05:40 +0500

    Update games-slotMachine.js

--- scripts/lang/russian/games/games-slotMachine.js
+++ scripts/lang/russian/games/games-slotMachine.js
@@ -1,6 +1,6 @@
 $.lang.register('slotmachine.result.start', '$1: [ $2 $3 $4 ] ');
 $.lang.register('slotmachine.result.win', ' +$1 ');
-$.lang.register('slotmachine.rewards.usage', 'Применение: !slot rewards [val1] [val2] [val3] [val4] [val5]. Currently: $1');
-$.lang.register('slotmachine.rewards.success', 'Updated rewards for the slot machine.');
-$.lang.register('slotmachine.emote.usage', 'Применение: !slot emotes [emote1] [emote2] [emote3] [emote4] [emote5]. Currently: $1');
-$.lang.register('slotmachine.emote.success', 'Updated emotes for the slot machine.');
+$.lang.register('slotmachine.rewards.usage', 'применение: !slot rewards [val1] [val2] [val3] [val4] [val5] (текущие: $1)');
+$.lang.register('slotmachine.rewards.success', 'вознаграждения слот-машины обновлены');
+$.lang.register('slotmachine.emote.usage', 'применение: !slot emotes [смайл 1] [смайл 2] [смайл 3] [смайл 4] [смайл 5] (текущие смайлы: $1)');
+$.lang.register('slotmachine.emote.success', 'смайлы для слотов обновлены');
