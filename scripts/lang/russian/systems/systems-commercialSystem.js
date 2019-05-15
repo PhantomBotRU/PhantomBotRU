e5b18563902dadd1dbb8bbad4fe9bd88e5a30725
PhantomBotRU <43908302+PhantomBotRU@users.noreply.github.com>
2019-05-16 03:28:10 +0500

    Update systems-commercialSystem.js

--- scripts/lang/russian/systems/systems-commercialSystem.js
+++ scripts/lang/russian/systems/systems-commercialSystem.js
@@ -15,13 +15,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$.lang.register('commercialsystem.usage', 'применение: !commercial (length_secs:30,60,90,120,150,180) [silent] --or-- !commercial autotimer');
+$.lang.register('commercialsystem.usage', 'применение: !commercial [30 / 60 / 90 / 120 / 150 / 180] [опционально: silent] или !commercial autotimer [опционально: количество минут, не менее 8] [опционально: 30, 60, 90, 120, 150 или 180] [опционально: текст сообщения]');
 $.lang.register('commercialsystem.run', 'запуск рекламы продолжительностью $1 сек');
 $.lang.register('commercialsystem.422', 'реклама может быть запущена только на партнёрских каналах во время стрима, продолжительностью 30, 60, 90, 120, 150 или 180 сек, с интервалом не менее 8 мин');
-$.lang.register('commercialsystem.autotimer.status-off', 'автоматическая реклама выключена'); // To enable: !commercial autotimer (interval_mins:8 or greater) (length_secs:30,60,90,120,150,180) [текст сообщения]
-$.lang.register('commercialsystem.autotimer.status-on', 'включена автоматическая реклама продолжительностью $1 сек и с интервалом $2 мин'); // To disable: !commercial autotimer off --or-- To add/change a message: !commercial autotimer message (message) --or-- To remove message: !commercial autotimer nomessage
-$.lang.register('commercialsystem.autotimer.bad-parm', 'продолжительность рекламы может быть только 30, 60, 90, 120, 150 или 180 сек, с интервалом не менее 8 мин');
-$.lang.register('commercialsystem.autotimer.status-on-msg', 'The message sent when an auto commercial starts: «$1»');
-$.lang.register('commercialsystem.autotimer.status-on-nomsg', 'No message is sent when an auto commercial starts');
+$.lang.register('commercialsystem.autotimer.status-off', 'автоматическая реклама выключена');
+$.lang.register('commercialsystem.autotimer.status-on', 'включена автоматическая реклама продолжительностью $1 сек и с интервалом $2 мин (для выключения наберите !commercial autotimer off, для добавления/редактирования сообщения наберите !commercial autotimer message [текст сообщения], для удаления сообщения наберите !commercial autotimer nomessage)');
+$.lang.register('commercialsystem.autotimer.bad-parm', 'продолжительность рекламного блока может быть только 30, 60, 90, 120, 150 или 180 сек, с интервалом не менее 8 мин');
+$.lang.register('commercialsystem.autotimer.status-on-msg', '$1'); // в оригинале: The message sent when an auto commercial starts: $1
+$.lang.register('commercialsystem.autotimer.status-on-nomsg', ''); // в оригинале: No message is sent when an auto commercial starts
 $.lang.register('commercialsystem.autotimer.msg-set', 'установлено сообщение при срабатывании автоматической рекламы: «$1»');
 $.lang.register('commercialsystem.autotimer.msg-del', 'сообщение при срабатывании автоматической рекламы удалено');
